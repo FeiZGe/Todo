cac0961462a888f18f8d0f060c47ad651e474235
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-22 12:40:42 +0700

    Edit: color

--- components/input.tsx
+++ components/input.tsx
@@ -8,7 +8,7 @@ const Input = ({name, type, placeholder, value}: inputProps) => {
         type={type} 
         placeholder={placeholder} 
         value={value}
-        className="block w-full p-4 mx-2 border rounded-lg text-base bg-slate-600 border-slate-500 placeholder-slate-400 text-white"
+        className="block w-full p-4 mx-2 border rounded-lg text-base bg-slate-200 border-slate-200 placeholder-slate-400 text-black"
       />
     </div>
   )
