268531d761a7ab247f8518f94acf93d270c6edf2
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-22 11:45:27 +0700

    Update: input

--- components/input.tsx
+++ components/input.tsx
@@ -1,13 +1,17 @@
 import { inputProps } from "@/types"
 
-const input = ({name, type, placeholder, value}: inputProps) => {
+const Input = ({name, type, placeholder, value}: inputProps) => {
   return (
     <div>
-      <input name={name} type={type} placeholder={placeholder} value={value}
-      className="block w-full p-4 mx-2 border rounded-lg text-base bg-slate-600 border-slate-500 placeholder-slate-400 text-white"
+      <input 
+        name={name} 
+        type={type} 
+        placeholder={placeholder} 
+        value={value}
+        className="block w-full p-4 mx-2 border rounded-lg text-base bg-slate-600 border-slate-500 placeholder-slate-400 text-white"
       />
     </div>
   )
 }
 
-export default input
+export default Input
